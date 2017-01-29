74e3f579d9f52b79284e28f31a5f6d752a6af3af
plumcraft <olivier.lebhard@gmail.com>
2017-01-29 21:42:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-{ module: 'mmm-stocks', position: 'top_bar', config: { stocks: 'ENV_STOCKS' , updateInterval: 37000 }, classes: 'plumcraft' }
+{ module: 'mmm-stocks', position: 'top_bar', config: { stocks: 'ENV_STOCKS' , updateInterval: 37000 }, classes: 'plumcraft' },
